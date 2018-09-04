81b9b47d0ef0d3232f812055f00cc134b7d43e43
mrkwskiti <markdog_@hotmail.com>
2018-09-04 16:48:49 +0700

    Update Process method in RPNCalculatorEngine

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -10,8 +10,16 @@ namespace CPE200Lab1
     {
         public string Process(string str)
         {
-            // your code here
-            return "E";
+            // your code 
+            string[] parts = str.Split(' ');
+            if ((isNumber(parts[0]) && isNumber(parts[1]) && isOperator(parts[2])))
+            {
+                return calculate(parts[2], parts[0], parts[1], 4);
+            }
+            else
+            {
+                return "E";
+            }
         }
     }
 }
